9b3583877ff357054c5a55adaa10c5e3c387c636
rhayeksa <rhayeksads@gmail.com>
2026-02-04 08:41:12 +0700

    kalibrasi query ke db

--- Src/Api/V1/User/Util/Register.cs
+++ Src/Api/V1/User/Util/Register.cs
@@ -1,7 +1,8 @@
 namespace dotnet_webapi_rock_paper_scissors.Src.Api.V1.User.Util;
 
 using dotnet_webapi_rock_paper_scissors.Src.Util;
-
+using Npgsql;
+using dotnet_webapi_rock_paper_scissors.src.Configs;
 public static class UtilRegister
 {
     public static object MapUtilRegister(
@@ -13,11 +14,65 @@ public static class UtilRegister
         string role = "player"
     )
     {
+        NpgsqlConnection? conn = null;
+        NpgsqlTransaction? tx = null;
         try
         {
 
-            // TODO: insert DB, hashing password, dsb
+            conn = Configs.GetConnection();
+            conn.Open();
 
+            // 🔥 transaction begin
+            tx = conn.BeginTransaction();
+            // TODO: insert DB, hashing password, dsb
+            // 1) check username exists
+
+            // var count = conn.exceute(
+            //     $@"
+            //     SELECT count(1)
+            //     FROM {Configs.PostgresSchema}.tb_users
+            //     WHERE deleted_at IS NULL
+            //     AND username = @username
+            //     ",
+            //     new() { ["username"] = username }
+            // );
+            var cmd = new NpgsqlCommand(
+                $@"
+                SELECT role, gender
+                FROM {Configs.PostgresSchema}.tb_users
+                WHERE deleted_at IS NULL
+                AND username = @username
+                "
+                , conn, tx);
+            cmd.Parameters.AddWithValue("username", username);
+
+            string? roleDb = null;
+            string? genderDb = null;
+
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (!reader.Read())
+                    return Results.BadRequest(ApiResponse.Response(404, "user not found"));
+                roleDb = reader["role"]?.ToString();
+                genderDb = reader["gender"]?.ToString();
+            } // reader dipastikan close/dispose disini
+
+            Console.WriteLine($">>> role: {roleDb}");
+            Console.WriteLine($">>> gender: {genderDb}");
+
+            // ---
+
+            // result.Read();
+            // // Console.WriteLine($">>> result: {result}");
+            // var a = result["role"];
+            // var b = result["gender"];
+            // Console.WriteLine($">>> a: {a}");
+            // Console.WriteLine($">>> b: {b}");
+
+            // if (result["count"] > 0)
+            //     return Results.BadRequest(ApiResponse.Response(400, "username already exists"));
+
+            tx.Commit();
             return Results.Created(
                 uri: "/api/v1/user/register",
                 value: ApiResponse.Response(
@@ -28,8 +83,13 @@ public static class UtilRegister
         }
         catch (Exception ex)
         {
+            tx?.Rollback();
             Console.WriteLine($"Error (register post): {ex.Message}");
-            return Results.InternalServerError(ApiResponse.Response(500, "Internal server error"));
+            return Results.InternalServerError(ApiResponse.Response(code: 500));
+        }
+        finally
+        {
+            conn?.Close();
         }
 
     }
