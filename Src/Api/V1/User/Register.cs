c05efd404ac699e83c483c621faf2a04e11e3a43
rhayeksa <rhayeksads@gmail.com>
2026-02-06 10:26:18 +0700

    fixed register

--- Src/Api/V1/User/Register.cs
+++ Src/Api/V1/User/Register.cs
@@ -2,36 +2,37 @@ using System.ComponentModel.DataAnnotations;
 using dotnet_webapi_rock_paper_scissors.Src.Util;
 using dotnet_webapi_rock_paper_scissors.Src.Api.V1.User.Util;
 
-public sealed class Request
-{
-    [Required]
-    [MinLength(1)]
-    public string Name { get; set; } = "";
-
-    [Required]
-    [MinLength(3)]
-    [MaxLength(30)]
-    public string Username { get; set; } = "";
-
-    [Required]
-    [MinLength(6)]
-    public string Password { get; set; } = "";
-
-    [Range(1, 250)]
-    public int? Age { get; set; }
-
-    [RegularExpression("^(M|F)$", ErrorMessage = "gender must be 'M' or 'F'")]
-    public string? Gender { get; set; }
-
-    [RegularExpression("^(player|administrator)$", ErrorMessage = "role must be 'administrator' or 'player'")]
-    public string Role { get; set; } = "player";
-}
 
 public static class Register
 {
+    public class ReqRegister
+    {
+        [Required]
+        [MinLength(1)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Username { get; set; } = "";
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = "";
+
+        [Range(1, 250)]
+        public int? Age { get; set; }
+
+        [RegularExpression("^(M|F)$", ErrorMessage = "gender must be 'M' or 'F'")]
+        public string? Gender { get; set; }
+
+        [RegularExpression("^(player|administrator)$", ErrorMessage = "role must be 'administrator' or 'player'")]
+        public string Role { get; set; } = "player";
+    }
+
     public static RouteGroupBuilder MapRegister(this RouteGroupBuilder user)
     {
-        user.MapPost("/register", (Request body) =>
+        user.MapPost("/register", (ReqRegister body) =>
         {
             var ctx = new ValidationContext(body);
             var results = new List<ValidationResult>();
